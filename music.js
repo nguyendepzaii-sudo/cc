974ad21c4c53107c05f93f766cde2ce97feb6f52
sko <a.nguyen230908@gmail.com>
2026-09-22 17:39:07 +0800

    Fix filename formatting for a song in music.js

--- music.js
+++ music.js
@@ -4,7 +4,7 @@
   const songs = [
     "h3R3 - 忘不掉的你.flac",
     "nuts.flac",
-    "05. Anh Chưa Thương Em Đến Vậy Đâu.opus",
+    "Anh Chưa Thương Em Đến Vậy Đâu.opus",
     "水仙LONE - 我走以后 (鼓点版).m4a",
     "夏日尽头的我们 x 月光呀月光.mp3",
     "Jess Lee - 甲乙丙丁Strangers.flac"
