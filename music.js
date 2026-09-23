1da74af33aa7c2f1e09f6422941fff3a2907c8fb
sko <a.nguyen230908@gmail.com>
2026-09-23 17:42:19 +0800

    Add new track to music.js

--- music.js
+++ music.js
@@ -4,7 +4,8 @@ const MUSIC_LIBRARY = [
   { src: "水仙LONE - 我走以后 (鼓点版).m4a" },
   { src: "夏日尽头的我们 x 月光呀月光.mp3" },
   { src: "Jess Lee - 甲乙丙丁Strangers.flac" },
-  { src: "Ta - 没有你我该怎么办.flac" }
+  { src: "Ta - 没有你我该怎么办.flac" }，
+  { src: "Renran - 无人之岛.m4a" },
 ];
 
 const parseTrackMetadata = (source) => {
