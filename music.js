820368616605874942e48576eb629f94014684ed
sko <a.nguyen230908@gmail.com>
2026-09-23 23:23:00 +0800

    Update music.js

--- music.js
+++ music.js
@@ -6,11 +6,12 @@ const MUSIC_LIBRARY = [
   "Jess Lee - 甲乙丙丁Strangers.flac",
   "Ta - 没有你我该怎么办.flac",
   "Renran - 无人之岛.m4a",
-  "鄒沛沛 & Pank 沉溺(你让我的心不再结冰) - Single (2023) . 沉溺(你让我的心不再结冰).m4a"
+  "鄒沛沛 & Pank 沉溺(你让我的心不再结冰) - Single (2023) . 沉溺(你让我的心不再结冰).m4a",
   {
     src: "https://pub-a6f896b739e543b7a5a3f838dd05edf9.r2.dev/h3R3%20-%20%E4%BA%B2%E7%88%B1%E7%9A%84.flac",
     // Overridden manually: filename's raw " - Single (2023)" segment doesn't split cleanly into artist/title.
-    
+    title: "亲爱的",
+    artist: "h3R3"
   },
   "https://pub-a6f896b739e543b7a5a3f838dd05edf9.r2.dev/%E6%88%91%E7%9A%84%E6%AD%8C%E5%A3%B0%E9%87%8C%20(DJ%E7%87%83%E6%9B%B2)%EF%BD%9C%E2%80%9C%E4%BD%A0%E5%AD%98%E5%9C%A8%E6%88%91%E6%B7%B1%E6%B7%B1%E7%9A%84%E8%84%91%E6%B5%B7%E9%87%8C%E2%80%9D.mp3",
   "https://pub-a6f896b739e543b7a5a3f838dd05edf9.r2.dev/LBI%20-%20%E5%8F%AF%E6%88%91%E5%8F%AA%E6%98%AF%E6%B5%B7.flac",
